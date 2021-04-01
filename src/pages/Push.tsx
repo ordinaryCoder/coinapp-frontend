20c69254d1fe5d634c4f67cb390c86215aabfc0c
premraj <premrajcser@gmail.com>
2021-04-01 20:01:38 +0530

    push notification

--- src/pages/Push.tsx
+++ src/pages/Push.tsx
@@ -2,39 +2,18 @@ import firebase from "firebase";
 import React from "react";
 
 function Push() {
-<<<<<<< HEAD
-
     React.useEffect(() => {
-
-        const msg = firebase.messaging();
-        msg.requestPermission().then(() => {
-            return msg.getToken();
-
-
-        }).then((data) => {
-            console.log("token", data)
-
-        })
-    })
+        getToken();
+    }, []);
+    const getToken = async () => {
+        const token = await firebase.messaging().getToken();
+        console.log("token", token);
+    };
     return (
         <div>
             <h1>hi push</h1>
         </div>
-    )
-=======
-  React.useEffect(() => {
-    getToken();
-  }, []);
-  const getToken = async () => {
-    const token = await firebase.messaging().getToken();
-    console.log("token", token);
-  };
-  return (
-    <div>
-      <h1>hi push</h1>
-    </div>
-  );
->>>>>>> 4ae66e79e432bcb305ab3c6fcceff6f2433a3c16
+    );
 }
 
 export default Push;
